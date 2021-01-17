2ab79e4c768d449503357a8da9cf84d7cb65bbdb
Mauro Filho <mauro.filho@gmail.com>
2021-01-17 18:57:29 -0300

    heroku support

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from eve import Eve
 from calculator import Calculator
 from repository import Repository
+import os
 
 
 def on_insert_matches(items):
@@ -15,6 +16,16 @@ def on_insert_matches(items):
         results.append(calculator.execute(task))
     player['results'] = results
 
+
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
 
 app = Eve()
 app.on_insert_matches += on_insert_matches
